7826ae10393ce150c8b977586c619a7c9c99ca69
Saajan Raj Pandey <saajan.pandey@ekbana.info>
2021-09-09 17:01:33 +0545

    Simple API CRUD

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id = null)
+    {
+        $products = Product::all()->where('status', 1);
+        return $products;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $product = new Product();
+        $product->product_name = $request->product_name;
+        $product->product_description = $request->product_description;
+        $product->product_price = $request->product_price;
+        $product->status = $request->status;
+        $result = $product->save();
+        if ($result) {
+            return ['Product Created'];
+        } else {
+            return ['Product Not Created'];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->product_name = $request->product_name;
+        $product->product_description = $request->product_description;
+        $product->product_price = $request->product_price;
+        $product->status = $request->status;
+        $result = $product->save();
+
+        if ($result) {
+
+            return ['Product Updated'];
+        } else {
+
+            return ['Product Not Updated'];
+        }
+    }
+
+    /**
+     * Remove the specified resource     from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $result = $product->delete();
+
+        if ($result) {
+
+            return ['Product Deleted'];
+        } else {
+
+            return ['Product Not Deleted'];
+        }
+    }
+}
---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_name' => 'required',
+            'product_description' => 'required|max:255',
+            'product_price' => 'required|numeric',
+            'status' => 'required|boolean'
+        ];
+    }
+}
